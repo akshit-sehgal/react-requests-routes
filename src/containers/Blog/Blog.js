1a0cb283bfb5015e9ae37d9a43ff3eb62aadde1a
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-07-07 21:01:13 +0530

    Implemented GET, POST, DELETE (REST) using Axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-
 class Blog extends Component {
+    state = {
+        posts:[],
+        selectedPostId:null,
+        error:false
+    };
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post =>{
+                return {
+                    ...post,
+                    author:'Aks'
+                }
+            });
+            this.setState((prevState,props)=>{
+                return {
+                    ...prevState,
+                    posts:updatedPosts
+                };
+            });
+        }).catch((error)=>{
+            this.setState({error:true});
+        });
+    }
+    postSelectedHandler = (id) =>{
+        this.setState((prevState, props)=>{
+            return {
+                ...prevState,
+                selectedPostId:id
+            }
+        })
+    }
     render () {
+        let posts = <p style={{textAlign:'center'}}>Something went wrong!</p>;
+        if(!this.state.error){
+        posts =this.state.posts.map((post)=>{
+            return (
+                <Post 
+                title={post.title}
+                key={post.id}
+                author={post.author}
+                clicked={()=>this.postSelectedHandler(post.id)}
+                />
+            );
+        })
+    }
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
