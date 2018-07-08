fa67be1bbe26cfa9958cdfaca77728c3fbccfdad
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-07-08 12:52:48 +0530

    Setting up Routing and Restructuring

---
+++ src/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers:{
+        'okay':'okay_world'
+    }
+});
+instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN_FROM_INSTANCE';
+instance.interceptors.request.use(request =>{
+    console.log(request);
+    return request;
+    }, error =>{
+        console.log(error);
+        return Promise.reject(error);
+    });
+export default instance;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,69 +1,24 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import axios from '../../axios';
+import Posts from './Posts/Posts';
 import './Blog.css';
 class Blog extends Component {
-    state = {
-        posts:[],
-        selectedPostId:null,
-        error:false
-    };
-    componentDidMount(){
-        axios.get('/posts').then((response)=>{
-            const posts = response.data.slice(0,4);
-            const updatedPosts = posts.map(post =>{
-                return {
-                    ...post,
-                    author:'Aks'
-                }
-            });
-            this.setState((prevState,props)=>{
-                return {
-                    ...prevState,
-                    posts:updatedPosts
-                };
-            });
-        }).catch((error)=>{
-            console.log(error);
-            this.setState({error:true});
-        });
-    }
-    postSelectedHandler = (id) =>{
-        this.setState((prevState, props)=>{
-            return {
-                ...prevState,
-                selectedPostId:id
-            }
-        })
-    }
+    
+    
     render () {
-        let posts = <p style={{textAlign:'center'}}>Something went wrong!</p>;
-        if(!this.state.error){
-        posts =this.state.posts.map((post)=>{
-            return (
-                <Post 
-                title={post.title}
-                key={post.id}
-                author={post.author}
-                clicked={()=>this.postSelectedHandler(post.id)}
-                />
-            );
-        })
-    }
+        
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/new-post">New Post</a></li>
+                    </ul>
+                </nav>
+            </header>
+                
+                <Posts/>
             </div>
         );
     }
