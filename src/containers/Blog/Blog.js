0015ca9dff650941e3533d5f3f0b112118341993
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-07-08 19:09:12 +0530

    Using Redirects

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import './Blog.css';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {    
@@ -30,12 +30,17 @@ class Blog extends Component {
                 </nav>
             </header>
             <Switch>           
-            <Route 
-            path="/new-post" 
-            component={NewPost}/>
+                <Route 
+                path="/new-post" 
+                component={NewPost}/>
                 <Route 
                 path="/posts"
                 component={Posts}/>
+                <Redirect from="/" to="/posts"/>                
+                {/*<Route
+                path="/"
+                component={Posts}/>*/}
+
             </Switch>
             </div>
         );
